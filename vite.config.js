a65bf1c7ea643b29b0ebedb619f5eab56b60e4f4
mjlemon1992 <mjlemon1992@gmail.com>
2026-04-16 22:50:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
+  root: __dirname,
   build: {
-    outDir: 'build'
-  },
-  esbuild: {
-    include: /\.[jt]sx?$/,
-    exclude: []
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
+    outDir: path.resolve(__dirname, 'build'),
+    emptyOutDir: true
   }
 })
