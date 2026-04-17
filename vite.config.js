9511ab078bd93abb05a245365af5468ce2bf82ed
mjlemon1992 <mjlemon1992@gmail.com>
2026-04-16 22:49:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: './index.html'
-    }
+    outDir: 'build'
   },
-  logLevel: 'info'
+  esbuild: {
+    include: /\.[jt]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
 })
